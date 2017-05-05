53dfa68a3c5fc16644a153ee91c911c73fabc920
guossnh <guossnh@gmail.com>
2017-05-05 19:56:58 +0800

    add new program

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+    <!--这里因为下部遮挡故意添加空白-->
+    <div class="zhedangjiachang"></div>
+    <!--这里是页面底部固定栏-->
+    <div class="weui-tabbar">
+        <div class="weui-navbar">
+           <div class="weui-navbar__item"><a href="#biaodan" class="weui-btn weui-btn_warn">在线下单</a></div>
+           <div class="weui-navbar__item"><a href="tel:400-867-3001" class="weui-btn weui-btn_warn">拨打电话</a></div>
+        </div>
+    </div>
+</body>
+</html>
+?>
---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+<html>
+<header>
+    <!--设置页面适应手机访问-->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--设置页面的编码方式-->
+    <meta charset="UTF-8">
+    <title></title>
+    <!--这是评论区的样式文件-->
+    <link href="style/pinglun.css" type="text/css" rel="stylesheet">
+    <!--这是页面前段框架使用微信的前端框架-->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/weui/1.1.1/style/weui.min.css"/>
+    <!--这是页面自己写的样式文件-->
+    <link rel="stylesheet" type="text/css" href="style/man.css" />
+
+</header>
+<body>
+?>
